1abd963f0c4a19c6e93573574582c02d5e76550e
raghav18gupta <18raghavgupta@gmail.com>
2018-02-07 22:47:17 +0530

    pytohn-advance

--- soCkets/UDPclient1.py
+++ soCkets/UDPclient1.py
@@ -2,14 +2,15 @@ import socket
 
 def Main():
     host = '127.0.0.1'
-    port = 5012
+    port = 7468
+    server = (host, 7400)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((host, port))
 
     message = input('-> ')
     while message != 'quit':
-        s.sendto(message.encode('utf-8'), (host, port))
+        s.sendto(message.encode('utf-8'), server)
         data, addr = s.recvfrom(1024)
         data = data.decode('utf-8')
         print('recived from server', data)
--- soCkets/UDPserver1.py
+++ soCkets/UDPserver1.py
@@ -2,12 +2,12 @@ import socket
 
 def Main():
     host = '127.0.0.1'
-    port = 5012
+    port = 7400
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((host, port))
-
     print('server started')
+
     while True:
         data, addr = s.recvfrom(1024)
         data = data.decode('utf-8')
