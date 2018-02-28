79fad24cfcd9fcf6b61a40e7b47e33860c56e389
raghav18gupta <18raghavgupta@gmail.com>
2018-02-28 12:58:20 +0530

    python-advance

--- others/raspi-ipaddr/raspi-ip.py
+++ others/raspi-ipaddr/raspi-ip.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 from gtts import gTTS
 import subprocess
-import socket
-import time
+import os
 
+FNULL = open(os.devnull, 'w')
 IPAddr = subprocess.check_output("ip route get 8.8.8.8 | awk '{print $NF; exit}'", stdin=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT).decode('UTF-8').rstrip().split('.')
-print(IPAddr)
 
 txt = 'IP address of your device is{0}dot{1}dot{2}dot{3}'.format(*IPAddr)
 language = 'en'
@@ -13,11 +12,7 @@ language = 'en'
 ttsobj = gTTS(text=txt, lang=language, slow=False)
 ttsobj.save('IPAddr.mp3')
 
-
 while True:
-  say = subprocess.Popen('mpg123 IPAddr.mp3', stdin=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT)
-  while say.poll() != 0:
-    pass
-#print(txt)
-
-
+    say = subprocess.Popen('mpg123 IPAddr.mp3', stdin=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT, stdout=FNULL)
+    while say.poll() != 0:
+        pass
--- others/raspi-ipaddr/setup.py
+++ others/raspi-ipaddr/setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 import subprocess
+import os
 
-a = subprocess.Popen('pip3 install gTTS', stdin=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT)
-while a.poll() !=0:
-  pass
-subprocess.Popen('sudo apt-get install mpg123 -y', stdin=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT)
+FNULL = open(os.devnull, 'w')
+print('installing\n   -> gTTS\n   -> mpg123')
+a = subprocess.Popen('pip3 install gTTS', stdin=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT, stdout=FNULL)
+b = subprocess.Popen('sudo apt-get install mpg123 -y', stdin=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT, stdout=FNULL)
+
+while (a.poll() and b.poll()) is None:
+    pass
+
+print('setup complete')
